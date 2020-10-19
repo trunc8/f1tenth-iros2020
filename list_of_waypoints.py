cd39223683ee8b06f0793b60732a66648f125f83
Siddharth Saha <sahasiddharth611@gmail.com>
2020-10-19 20:20:03 +0530

    Read both the json files in sequence and obtain a single list of the global trajectory of waypoints

---
+++ list_of_waypoints.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# trunc8 did this
+
+import json
+
+if __name__ == '__main__':
+  list_of_waypoints = []
+  json_file_path1 = 'json_waypoints1.json'
+  with open(json_file_path1, 'r') as read_file:
+    list_of_waypoints1 = json.load(read_file)
+  json_file_path2 = 'json_waypoints2.json'
+  with open(json_file_path2, 'r') as read_file:
+    list_of_waypoints2 = json.load(read_file)
+  list_of_waypoints.extend(list_of_waypoints1)
+  print(len(list_of_waypoints))
+  list_of_waypoints.extend(list_of_waypoints2)
+  print(len(list_of_waypoints))
